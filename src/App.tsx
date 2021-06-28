c63ef3b07543e2e7a8ed58e0231baaa74bea503a
*Guilherme <gulhermecrad@gmail.com>
2021-06-28 17:56:13 -0300

    terceiro dia

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+<<<<<<< HEAD
 import { Home } from "./pages/Home";
 
 function App() {
   return (    
     <Home/>
+=======
+//import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+function App() {
+  return (    
+    <NewRoom/>
+>>>>>>> terceiro dia
     );    
 }
 
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -2,48 +2,32 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
-<<<<<<< HEAD
-=======
 import { Button } from '../components/Button';
 
->>>>>>> terceiro dia
 import '../styles/auth.scss';
 
-export function Home(){
+export function NewRoom(){
   return(
     <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />   
-<<<<<<< HEAD
-        <strong>Cire salas de Q&amp;A ao-vivo</strong>
-=======
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
->>>>>>> terceiro dia
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>      
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room">
-            <img src={googleIconImg} alt="Logo do google" />
-            Crie sua sala com o Google                     
-          </button>
-          <div className="separator">ou entre em uma sala</div>
+          <h2>Criar uma nova sala</h2>
           <form >
             <input
               type="text"
-              placeholder="Digite o código da sala"            
+              placeholder="Nome da sala"            
             />
-<<<<<<< HEAD
-            <button type="submit">
-              Entrar na sala
-            </button>
-=======
             <Button type="submit">
-              Entrar na sala
+              Criar sala
             </Button>
->>>>>>> terceiro dia
           </form>
+          <p>Quer entrar em uma sala existente? <a href="#">clique aqui</a> </p>
         </div>
       </main>
     </div>
